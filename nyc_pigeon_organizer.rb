50a7ff97c9eb27ea48759b0385888b93eb9a63e7
Adam Hardy <rpihardy@gmail.com>
2015-12-29 21:23:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  names = []
+  data.each { |descriptor, type|
+    type.each { |type_name, name_array|
+      names << name_array
+      }
+    }
+  names.flatten!.uniq!
+  new_hash = {}
+  idx = 0
+  while idx < names.size
+    new_hash[names[idx]] = {}
+    idx += 1
+  end
+  new_hash.each { |name, description|
+    data.each { |descriptor, type|
+      type.each { |type_name, name_array|
+        if name_array.include?(name)
+          if new_hash[name][descriptor] == nil
+            new_hash[name][descriptor] = [type_name.to_s]
+          else
+            new_hash[name][descriptor] << type_name.to_s
+          end
+        end
+        }
+      }
+    }
+  new_hash
 end
